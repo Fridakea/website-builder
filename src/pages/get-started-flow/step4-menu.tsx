f337e6adf5ab8b5b598d0418bef08a6292f80f5c
Fridakea <frbe0002@stud.kea.dk>
2025-04-28 23:29:08 +0200

    Step 4 menu categories and items impleneted

--- src/pages/get-started-flow/step4-menu.tsx
+++ src/pages/get-started-flow/step4-menu.tsx
@@ -11,6 +11,7 @@ import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
 const formSchema = z.object({
   menuCategory: z.string(),
 });
@@ -19,7 +20,7 @@ type FormData = z.infer<typeof formSchema>;
 
 export const Step4MenuPage = () => {
   const navigate = useNavigate();
-  const { menu, addMenuCategory, removeMenuCategory, addMenuItem } = useWebsiteInfoStore();
+  const { menu, addMenuCategory, removeMenuCategory, addMenuItem, removeMenuItem } = useWebsiteInfoStore();
   const [menuItemInputs, setMenuItemInputs] = useState<{ categoryName: string; value: string }[]>([]);
 
   const formObject = useForm<FormData>({
@@ -33,8 +34,14 @@ export const Step4MenuPage = () => {
     const newCategory = formObject.getValues("menuCategory");
     addMenuCategory(newCategory);
     setMenuItemInputs([...menuItemInputs, { categoryName: newCategory, value: "" }]);
-
     formObject.reset();
+  };
+
+  const addMenuItemHandler = (categoryName: string) => {
+    addMenuItem(categoryName, menuItemInputs.find((item) => item.categoryName === categoryName)?.value ?? "");
+
+    // Reset the input fields
+    setMenuItemInputs(menu.map((menuCategory) => ({ categoryName: menuCategory.name, value: "" })));
   };
 
   const onSubmit = async (values: FormData) => {
@@ -45,67 +52,81 @@ export const Step4MenuPage = () => {
   return (
     <Form {...formObject}>
       <form onSubmit={formObject.handleSubmit(onSubmit)}>
-        <div>
-          <FormField
-            control={formObject.control}
-            name="menuCategory"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Menu</FormLabel>
-                <FormControl>
-                  <Dialog>
-                    <DialogTrigger>Tilføj kategori</DialogTrigger>
+        <Accordion type="single" collapsible className="w-[400px]">
+          {" "}
+          <div>
+            <FormField
+              control={formObject.control}
+              name="menuCategory"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Menu</FormLabel>
+                  <FormControl>
+                    <Dialog>
+                      <DialogTrigger>Tilføj kategori</DialogTrigger>
 
-                    <DialogContent>
-                      <DialogHeader>
-                        <DialogTitle>Tilføj kategori</DialogTitle>
-                        <DialogDescription hidden>Tilføj en kategori til dit menukort</DialogDescription>
-                      </DialogHeader>
-                      <Label>Kategori navn</Label>
-                      <Input type="text" placeholder="Kategori navn" {...field} />
+                      <DialogContent>
+                        <DialogHeader>
+                          <DialogTitle>Tilføj kategori</DialogTitle>
+                          <DialogDescription hidden>Tilføj en kategori til dit menukort</DialogDescription>
+                        </DialogHeader>
+                        <Label>Kategori navn</Label>
+                        <Input type="text" placeholder="Kategori navn" {...field} />
 
-                      <DialogFooter>
-                        <DialogClose asChild>
-                          <Button type="button" onClick={addMenuCategoryHandler}>
-                            Tilføj
-                          </Button>
-                        </DialogClose>
-                      </DialogFooter>
-                    </DialogContent>
-                  </Dialog>
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-        </div>
-
-        <hr />
-        {menu.map((category) => (
-          <div key={category.name} className="border border-gray-300 rounded-md p-2 flex flex-col">
-            <div className="flex flex-row justify-between">
-              <h3>{category.name}</h3>
-              <Button onClick={() => removeMenuCategory(category.name)}>X</Button>
-            </div>
-            <hr className="my-2" />
-            <div>
-              <Input
-                type="text"
-                placeholder="Tilføj"
-                value={menuItemInputs.find((item) => item.categoryName === category.name)?.value}
-                onChange={(e) => setMenuItemInputs(menuItemInputs.map((item) => (item.categoryName === category.name ? { ...item, value: e.currentTarget.value } : item)))}
-              />
-              <Button type="button" onClick={() => addMenuItem(category.name, menuItemInputs.find((item) => item.categoryName === category.name)?.value ?? "")}>
-                Tilføj
+                        <DialogFooter>
+                          <DialogClose asChild>
+                            <Button variant="default" type="button" onClick={addMenuCategoryHandler}>
+                              Tilføj
+                            </Button>
+                          </DialogClose>
+                        </DialogFooter>
+                      </DialogContent>
+                    </Dialog>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <hr />
+          {menu.map((category) => (
+            <div key={category.name} className="flex flex-row justify-between">
+              <AccordionItem className="w-full" value={category.name}>
+                <AccordionTrigger>
+                  <div className="flex flex-row justify-between items-center">
+                    <h3>{category.name}</h3>
+                  </div>
+                </AccordionTrigger>
+                <AccordionContent>
+                  <div className="flex flex-row gap-2">
+                    <Input
+                      type="text"
+                      placeholder="Tilføj"
+                      value={menuItemInputs.find((item) => item.categoryName === category.name)?.value}
+                      onChange={(e) => setMenuItemInputs(menuItemInputs.map((item) => (item.categoryName === category.name ? { ...item, value: e.currentTarget.value } : item)))}
+                    />
+                    <Button type="button" onClick={() => addMenuItemHandler(category.name)}>
+                      Tilføj
+                    </Button>
+                  </div>
+                  <ul className="p-2 flex flex-col gap-1">
+                    {category.items.map((item) => (
+                      <li key={item.name} className="flex flex-row justify-between items-center">
+                        <p>{item.name}</p>
+                        <Button type="button" onClick={() => removeMenuItem(category.name, item.name)}>
+                          X
+                        </Button>
+                      </li>
+                    ))}
+                  </ul>
+                </AccordionContent>
+              </AccordionItem>
+              <Button variant="default" onClick={() => removeMenuCategory(category.name)}>
+                X
               </Button>
             </div>
-            <ul>
-              {category.items.map((item) => (
-                <li key={item.name}>{item.name}</li>
-              ))}
-            </ul>
-          </div>
-        ))}
+          ))}
+        </Accordion>
 
         {/* <Button type="button" onClick={() => navigate(-1)}>
           Tilbage
