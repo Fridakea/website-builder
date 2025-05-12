e1656209fc3fb2ddcf89901df5c3b26d27e8e02a
Fridakea <frbe0002@stud.kea.dk>
2025-05-12 23:44:26 +0200

    Update website-builder.tsx

--- src/pages/website-builder.tsx
+++ src/pages/website-builder.tsx
@@ -1,4 +1,6 @@
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
 import { ERoutes } from "@/main";
 import { HeroSection } from "@/features/website-builder/components/hero-section";
 import { useWebsiteInfoStore } from "@/stores/website-info-store";
@@ -10,31 +12,49 @@ import { FindUs } from "@/features/website-builder/components/find-us";
 import { ImageGallery } from "@/features/website-builder/components/image-gallery";
 import { Footer } from "@/features/website-builder/components/footer";
 import { Contact } from "@/features/website-builder/components/contact";
+import { ChevronRight, ChevronLeft } from "lucide-react";
 
 export const WebsiteBuilderPage = () => {
   const navigate = useNavigate();
   const { theme, setTheme, imageGallery, menu } = useWebsiteInfoStore();
 
   const choosenTheme = themeOptions.find((option) => option.theme === theme);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
 
   if (!choosenTheme) {
-    setTheme(ETheme.COLORFUL);
+    setTheme(ETheme.CLASSIC);
     return <div>No theme found</div>;
   }
 
   return (
-    <div className="w-full" style={{ fontFamily: choosenTheme.fontFamily, backgroundColor: choosenTheme.backgroundColor, color: choosenTheme.textColor }}>
-      <HeroSection />
+    <div
+      className="w-full flex flex-row"
+      style={{ fontFamily: choosenTheme.fontFamily, backgroundColor: choosenTheme.backgroundColor, color: choosenTheme.textColor }}
+    >
+      <div className="flex-1">
+        <div className="relative top-0 right-0"></div>
 
-      <div className="max-w-content overflow-x-hidden mx-auto px-5 sm:px-10 my-16 sm:my-20 flex flex-col gap-16 sm:gap-20">
-        {menu.length > 0 && <Menu />}
-        {imageGallery.length > 0 && <ImageGallery />}
-        <Contact />
-        <FindUs />
+        <HeroSection />
 
-        <Button onClick={() => navigate(ERoutes.HOME)}>Forside</Button>
+        <div className="max-w-content overflow-x-hidden mx-auto px-5 sm:px-10 my-16 sm:my-20 flex flex-col gap-16 sm:gap-20">
+          {menu.length > 0 && <Menu />}
+          {imageGallery.length > 0 && <ImageGallery />}
+          <Contact />
+          <FindUs />
+
+          <Button onClick={() => navigate(ERoutes.HOME)}>Forside</Button>
+        </div>
+        <Footer />
       </div>
-      <Footer />
+      <div className="bg-green-400 relative transition-all duration-300 ease-in-out" style={{ width: isSidebarOpen ? "300px" : "0px" }}>
+        <div className="bg-red-400 z-100 fixed top-0 right-0 h-full transition-all duration-300 ease-in-out" style={{ width: isSidebarOpen ? "300px" : "0px" }}>
+          <button onClick={() => setIsSidebarOpen(!isSidebarOpen)} className="absolute top-3 -left-6 bg-green-400 size-6">
+            {isSidebarOpen ? <ChevronRight /> : <ChevronLeft />}
+          </button>
+
+          <h1>Sidebar</h1>
+        </div>
+      </div>
     </div>
   );
 };
