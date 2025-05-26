2b2a9ebbae759259a6d6f6ef3724015182c8fefc
Fridakea <143487024+Fridakea@users.noreply.github.com>
2025-05-27 00:11:24 +0200

    fonts adjusted

--- src/features/landing-page/components/selling-section.tsx
+++ src/features/landing-page/components/selling-section.tsx
@@ -12,9 +12,9 @@ const stats = [
 
 export const SellingSection = () => {
   return (
-    <div className="max-w-content mx-auto relative z-10 rounded-4xl bg-background-tint py-16 px-4 sm:p-20 flex flex-col gap-8 sm:gap-16">
-      <div className="flex flex-col-reverse gap-10 sm:flex-row">
-        <div className="flex-[1.5] flex flex-col items-start justify-center gap-4 sm:gap-5">
+    <div className="max-w-content mx-auto relative z-10 rounded-4xl bg-background-tint py-16 px-4 sm:px-8 lg:p-20 flex flex-col gap-8 sm:gap-16">
+      <div className="flex flex-col-reverse gap-10 sm:gap-5 md:gap-10 sm:flex-row">
+        <div className="flex-1 md:flex-[1.5] flex flex-col items-start justify-center gap-4 sm:gap-5">
           <div className="px-8 sm:px-0">
             <h6 className="text-primary mb-2">Opstart</h6>
             <h2>Hjemmesider skræddersyet til dit spisested</h2>
@@ -28,12 +28,17 @@ export const SellingSection = () => {
           <p>Alt du skal gøre at at følge disse 5 steps og så kan du have en hjemmeside der er designet til at være optimal til dit spisested.</p>
         </div>
 
-        <div className="flex-1">
-          <img src={websiteBuilderImg} alt="Spisesteder.com's hjemmeside bygger billede vælger" className="rounded-2xl" />
+        <div className="flex-1 rounded-2xl flex items-center justify-center">
+          <img
+            src={websiteBuilderImg}
+            width={500}
+            alt="Spisesteder.com's hjemmeside bygger billede vælger"
+            className="w-full rounded-2xl border-2 border-muted/50 shadow-lg"
+          />
         </div>
       </div>
 
-      <dl className="p-6 sm:p-8 gap-1 bg-card grid grid-cols-2 overflow-hidden rounded-2xl text-center sm:grid-cols-3 lg:grid-cols-6">
+      <dl className="p-6 sm:p-8 gap-1 bg-card/75 grid grid-cols-2 overflow-hidden rounded-2xl text-center sm:grid-cols-3 lg:grid-cols-6">
         {stats.map((stat) => (
           <div key={stat.id} className={twMerge("flex flex-col p-2", stat.textColor)}>
             <dt className="text-sm/6 font-semibold text-muted-foreground">{stat.name}</dt>
